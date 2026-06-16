58d42005f52751156c3d36f3657a45500be4771a
Hari07 <harinisj.24csd@kongu.edu>
2026-06-16 16:10:21 +0530

    added

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+const moongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await moongoose.connect(process.env.MONGO_URL);
+        console.log('MongoDB connected'+ process.env.PORT);
+    } catch (error) {
+        console.log(error);
+    }
+    };
+    module.exports = connectDB;
