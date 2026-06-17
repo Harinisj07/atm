ee7b149236ddd9babdedd0919435d3401d134168
Hari07 <harinisj.24csd@kongu.edu>
2026-06-17 11:37:09 +0530

    Fix MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const moongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await moongoose.connect(process.env.MONGO_URL);
+        await moongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected'+ process.env.PORT);
     } catch (error) {
         console.log(error);
